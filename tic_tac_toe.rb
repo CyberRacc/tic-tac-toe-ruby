553096fbdcc2fb86686960a11b4bbe12f53a4823
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-02-25 23:07:23 +0000

    Added Player class and initialize methods.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 # typed: true
 
-# Contains the logic for the game
+# TicTacToe Class
 class TicTacToe
+  def initialize
+    welcome_message
+    player = Player.new(user_name, user_symbol)
+    cpu = Player.new('CPU', player.symbol == 'X' ? 'O' : 'X')
+    puts player
+    puts cpu
+  end
+
   @game_board = [
     ['-', '-', '-'],
     ['-', '-', '-'],
@@ -12,8 +20,42 @@ class TicTacToe
   def show_board
     @game_board.each { |row| p row }
   end
+
+  private
+
+  def update_board(row, index, symbol)
+    @game_board[row][index] = symbol
+  end
+
+  def welcome_message
+    puts "Welcome to CyberRacc's Tic Tac Toe game in Ruby!"
+  end
+
+  def user_name
+    puts "What's your name?"
+    print 'My name is: '
+    gets.chomp.to_s
+  end
+
+  def user_symbol
+    print 'Choose your symbol (X)/(O): '
+    print 'Choose your symbol (X)/(O): ' while gets.chomp.upcase != 'X'
+  end
 end
 
-game = TicTacToe.new
+# Define base class for Player and Player functions
+class Player
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+    display_player_info
+  end
 
-game.show_board
+  def display_player_info
+    puts "#{@name}'s Symbol: #{@symbol}"
+  end
+
+  attr_reader :name, :symbol
+end
+
+TicTacToe.new
